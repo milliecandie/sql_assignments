4f66d17cbd682c4527aee45aa621cc9c9d87a9cd
Millicent Nabututu Makokha <candymillie56@gmail.com>
2025-08-14 12:08:03 +0300

    Delete drop_database_assignment_millicent.sql

--- drop_database_assignment_millicent.sql
+++
@@ -1,8 +0,0 @@
--- PLP Assignment by Millicent Makokha
--- Task: Drop a database called demo
-
--- Step 1: View all existing databases (optional)
-SHOW DATABASES;
-
--- Step 2: Drop the demo database
-DROP DATABASE demo;
